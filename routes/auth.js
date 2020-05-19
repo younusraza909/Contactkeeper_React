1655cf6aad4fcd7328b5f63227c6f7fc8e4e856b
younusraza909 <younusraza909@gmail.com>
2020-05-19 08:18:20 -0700

    login route setup to compare hashed password and sending back jwt

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
+const User = require("../models/User");
 
 //@route  GET api/users
 //@desc   Logged in user
@@ -13,8 +18,53 @@ router.get("/", (req, res) => {
 //@desc   auth user & get token
 //@access  Public
 
-router.post("/", (req, res) => {
-  res.send("TO Authenticate user");
-});
+router.post(
+  "/",
+  [
+    check("email", "Please Include Valid Email").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ msg: "Invalid Credentials" });
+      }
+      // the password stored in database is hashed that is why we use bcrypt.compare method to compare normal password and hash password
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Invalid Credentials...!" });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.err(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
