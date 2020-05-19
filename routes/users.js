1486925c2ee3e3d88d590d62dff154f846d5a9bc
younusraza909 <younusraza909@gmail.com>
2020-05-19 03:15:59 -0700

    Jwt created and sent throught api

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator/check");
 const bcrypt = require("bcryptjs");
 
@@ -49,7 +51,23 @@ router.post(
 
       await user.save(); //return promises
 
-      res.send("User Saved");
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json(token);
+        }
+      );
     } catch (err) {
       console.log(err.message);
       res.status(500).send("Server Error");
