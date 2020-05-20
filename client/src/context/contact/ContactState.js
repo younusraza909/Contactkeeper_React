a642f360ed21ec872961d750edad312a4026e165
younusraza909 <younusraza909@gmail.com>
2020-05-20 11:46:52 -0700

    Implementing Context Api

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,70 @@
+import React, { useReducer } from "react";
+import uuid from "uuid";
+import contactContext from "./contactContext";
+import contactReducer from "./contactReducer";
+
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  UPDATE_CONTACT,
+  CLEAR_CURRENT,
+  FILTER_CONTACT,
+  CLEAR_FILTER,
+} from "../types";
+
+const ContactState = (props) => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: "Jill Johnson",
+        email: "jill@gmail.com",
+        phone: "111-111-1111",
+        type: "personal",
+      },
+      {
+        id: 2,
+        name: "Jack Johnson",
+        email: "jack@gmail.com",
+        phone: "222-222-2222",
+        type: "personal",
+      },
+      {
+        id: 1,
+        name: "Smith Johnson",
+        email: "smith@gmail.com",
+        phone: "333-333-3333",
+        type: "professional",
+      },
+    ],
+  };
+
+  const { state, dispatch } = useReducer(contactReducer, initialState);
+
+  //Add Contact
+
+  //Delete Contact
+
+  //Set Current Contact
+
+  //CLear Contact current
+
+  //update contact
+
+  //filter contact
+
+  //remove filter
+
+  return (
+    <contactContext.Provider
+      vaue={{
+        contacts: state.contacts,
+      }}
+    >
+      {props.children}
+    </contactContext.Provider>
+  );
+};
+
+export default ContactState;
